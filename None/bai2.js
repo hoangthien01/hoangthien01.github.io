1727aef18640db45fe77f7b36776b8c8e68f8a07
hoangthien01 <64721279+hoangthien01@users.noreply.github.com>
2021-04-05 16:53:57 +0700

    Delete None directory

--- None/bai2.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-var d = today.getDate();
-var m = today.getMonth();
-var y = today.getFullYear();
-
-console.log(d + "/" + m + "/" + y);
-console.log(m + "/" + d + "/" + y);
