fa05418ecfe44eea8bf6aa8740d2010a1f1efe0e
hoangthien <thienho549@gmail.com>
2021-06-27 11:58:56 +0700

    hmm

--- Vue/Vue_cli/blog/src/layout/unauth.vue
+++ Vue/Vue_cli/blog/src/layout/unauth.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="unauth-layout">
-      <!-- <Nav></Nav>    -->
-      <!-- <Login></Login> -->
       <router-view></router-view>
   </div>
 </template>
 <script>
-// import Nav from '../components/common/Nav.vue'
-// import Login from '../components/Login.vue'
+
 export default {
-    components: {
-        // Login,
-        // Nav
-    }
+
 }
 </script>
 
